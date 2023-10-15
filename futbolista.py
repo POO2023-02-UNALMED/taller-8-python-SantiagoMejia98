a01e21508736a616d1985434c27f7ecc29488510
SantiagoMejia98 <141966190+SantiagoMejia98@users.noreply.github.com>
2023-10-15 16:28:37 -0500

    Solucion

---
+++ futbolista.py
@@ -0,0 +1,34 @@
+from deportista import Deportista
+from persona import Persona
+
+
+class Futbolista(Persona, Deportista):
+    listaFutbolistas = []
+    def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
+        Persona.__init__(nombre, edad, altura, sexo)
+        Deportista.__init__(añosPracticando, "Futbol")
+        self._golesMarcados = golesMarcados
+        self._tarjetasRojas = tarjetasRojas
+        self._piernaHabil = piernaHabil
+        Futbolista.listaFutbolistas.append(self)
+
+    def getGolesMarcados(self):
+        return self._golesMarcados
+    
+    def setGolesMarcados(self, golesMarcados):
+        self._golesMarcados = golesMarcados
+
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+    
+    def setTarjetasRojas(self, tarjetasRojas):
+        self._tarjetasRojas = tarjetasRojas
+
+    def getPiernaHabil(self):
+        return self._piernaHabil
+    
+    def setPiernaHabil(self, piernaHabil):
+        self._piernaHabil = piernaHabil
+
+    def __str__(self):
+        return "Mi nombre es " + self.getNombre() + " soy profesional en el deporte " + self.getDeporte() + " Tengo " + str(self.getEdad()) + " años de edad y llevo " + str(self.getAñosPracticando()) + " años en el deporte"
